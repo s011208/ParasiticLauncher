675482e72f4f9f5dff3300dfb8f01e3ac2b25cdc
Yen-Hsun, Huang <s011208@gmail.com>
2016-02-03 08:32:30 +0800

    Initial commit

---
+++ app/src/main/java/yhh/bj4/parasitic/launcher/widgets/ParasiticWidgetProvider.java
@@ -0,0 +1,9 @@
+package yhh.bj4.parasitic.launcher.widgets;
+
+import android.appwidget.AppWidgetProvider;
+
+/**
+ * Created by yenhsunhuang on 2016/2/2.
+ */
+public class ParasiticWidgetProvider extends AppWidgetProvider {
+}
---
+++ app/src/main/java/yhh/bj4/parasitic/launcher/widgets/configure/ParasiticWidgetConfigureActivity.java
@@ -0,0 +1,9 @@
+package yhh.bj4.parasitic.launcher.widgets.configure;
+
+import android.app.Activity;
+
+/**
+ * Created by yenhsunhuang on 2016/2/2.
+ */
+public class ParasiticWidgetConfigureActivity extends Activity {
+}
