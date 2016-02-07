18133ebac95f2d7b49c220769ee51f93f4f83a10
Yen-Hsun, Huang <s011208@gmail.com>
2016-02-07 22:35:22 +0800

    add allapps widget configure

---
+++ app/src/main/java/yhh/bj4/parasitic/launcher/widgets/allapps/AllappsWidgetConfiguration.java
@@ -0,0 +1,16 @@
+package yhh.bj4.parasitic.launcher.widgets.allapps;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+/**
+ * Created by yenhsunhuang on 2016/2/7.
+ */
+public class AllappsWidgetConfiguration extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Toast.makeText(this, "hi", Toast.LENGTH_LONG).show();
+    }
+}
