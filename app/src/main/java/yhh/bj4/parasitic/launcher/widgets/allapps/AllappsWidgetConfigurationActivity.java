3d2bb66a19f73b0c6af54716a7af3877a705edcb
Yen-Hsun, Huang <s011208@gmail.com>
2016-02-08 13:09:07 +0800

    add appwidget id & result for configure activity

--- app/src/main/java/yhh/bj4/parasitic/launcher/widgets/allapps/AllappsWidgetConfigurationActivity.java
+++ app/src/main/java/yhh/bj4/parasitic/launcher/widgets/allapps/AllappsWidgetConfigurationActivity.java
@@ -1,6 +1,8 @@
 package yhh.bj4.parasitic.launcher.widgets.allapps;
 
 import android.app.Activity;
+import android.appwidget.AppWidgetManager;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 
@@ -13,13 +15,36 @@ import yhh.bj4.parasitic.launcher.utils.iconpack.IconPackListDialog;
 public class AllappsWidgetConfigurationActivity extends Activity implements IconPackListDialog.Callback {
     private static final String TAG = "AllappsWidgetConfigure";
     private static final boolean DEBUG = true;
+    private int mAppWidgetId = AppWidgetManager.INVALID_APPWIDGET_ID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Intent intent = getIntent();
+        Bundle extras = intent.getExtras();
+        if (extras != null) {
+            mAppWidgetId = extras.getInt(
+                    AppWidgetManager.EXTRA_APPWIDGET_ID,
+                    AppWidgetManager.INVALID_APPWIDGET_ID);
+        }
+        if (DEBUG) {
+            Log.d(TAG, "mAppWidgetId: " + mAppWidgetId);
+        }
+        if (mAppWidgetId == AppWidgetManager.INVALID_APPWIDGET_ID) {
+            finish();
+        }
+
         setContentView(R.layout.all_apps_widget_configuration_activity);
         getFragmentManager().beginTransaction().replace(R.id.prefs_fragment_container,
                 new AllappsWidgetConfigurePreference()).commit();
+    }
+
+    @Override
+    public void onBackPressed() {
+        Intent resultValue = new Intent();
+        resultValue.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, mAppWidgetId);
+        setResult(RESULT_OK, resultValue);
+        super.onBackPressed();
     }
 
     @Override
